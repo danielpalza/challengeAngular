81a43c4bf636851884a22b580f560ea48d4ab176
Daniel Palza <danielpalza@hotmail.com>
2023-10-26 19:16:47 -0300

    ng-bootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenericTableComponent } from './components/generic-table/generic-table.component';
 import { GenericFormComponent } from './components/generic-form/generic-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GenericTableComponent,
-    GenericFormComponent
+    GenericFormComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
