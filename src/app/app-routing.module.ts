b4925b5005df2471d8fac5296a12777851a5c991
Daniel Palza <danielpalza@hotmail.com>
2023-10-26 19:16:30 -0300

    sidenav created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GenericFormComponent } from './components/generic-form/generic-form.component';
+import { GenericTableComponent } from './components/generic-table/generic-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: GenericFormComponent },
+  { path: 'table', component: GenericTableComponent },
+  {path: '', redirectTo: '/form', pathMatch: 'full'},
+  {path: '**', redirectTo: '/form', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
